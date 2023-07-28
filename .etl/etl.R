fbd46c2a6726644a948c76da17c4667e5eff4fc0
rl627 <rl627@drexel.edu>
2023-07-28 09:22:23 -0400

    restored scatter demo etl pipeline #2

--- .etl/etl.R
+++ .etl/etl.R
@@ -1,4 +1,7 @@
-#' This script will generate the simulated data
+#' This script will generate the following outputs:
+#'   1. county_topo.json (legacy: geo_lad2021.json)
+#'   2. data_place.csv (legacy: data_region.csv)
+#'   2. data_county.csv (legacy: data_district.csv)
 
 { # Setup -------------------------------------------------------------------
   
@@ -25,35 +28,172 @@
 }
 
 
-{# US Trends ---------------------------------------------------------------
+{ # EDA -----------------------------------------------------------
+  #' Examine and reproduce original items
   
-  df_us = read.csv("raw/NCHS_-_Death_rates_and_life_expectancy_at_birth.csv") %>% as_tibble() %>% 
-    janitor::clean_names() %>% 
-    filter(race == "All Races", sex == 'Both Sexes',
-           year > 1980) %>% 
-    select(year, 
-           le = average_life_expectancy_years) %>% 
-    mutate(group = 'US')
+  { # geo_lad2021.json --------------------------------------------------------
+    
+    sfa = geojsonio::topojson_read("../public/data/geo_lad2021.json")
+    glimpse(sfa)
+    sfa %>% geojsonio::topojson_write(
+      file = "../public/data/geo_lad2021_uhc.json",
+      object_name  = 'geog')
+    
+  }
+  
+  { # data_region.csv ------------------------------------------------------------------
+    ## region (12)
+    
+    df_region = read_csv("../public/data/data_region.csv")
+    df_region %>% glimpse()
+    df_region %>% count(code, name)  
+  }
+  
+  
+  { # data_district.csv -------------------------------------------------------
+    
+    ## district (374)
+    df_district = read_csv("../public/data/data_district.csv")
+    df_district %>% glimpse()
+    df_district %>% count(code, name)
+    
+  }
   
 }
 
-
-{ # Philly Simulated --------------------------------------------------------
-  
-  df_philly =  read.csv("raw/le_gpt_philly.csv") %>% as_tibble() %>% 
-    mutate(group = "Philadelphia")
-  
-  df_data = bind_rows(df_philly, df_us)
-  df_data %>% 
-    ggplot(aes(x =year, y = le, group = group, color = group))+
-    geom_line()+
-    ylim(60,80)
+{ # Outputs --------------------------------------------
   
   
-}
-
-{ # Export ------------------------------------------------------------------
+  region_tmp = 'Northeast'
+  division_tmp =  "Middle Atlantic"
   
-  df_data %>% jsonlite::write_json("../public/data/data_trends.json")
+  vec__state_abbr = xwalk_state %>% 
+    # filter(division_name == division_tmp) %>% 
+    filter(region_name == region_tmp) %>% 
+    pull(state_abbr)
+  
+  { ##  county_topo.json ----------------------------------------------------------------
+    
+    
+    ### Op
+    ### - select only PA, DE, MD
+    ### - rename as per original data structure
+    
+    sf_init = sf_county_seed %>% 
+      left_join(xwalk_state) %>% 
+      filter(state_abbr%in%vec__state_abbr)
+    
+    sf_uhc = sf_init %>%  
+      mutate(id = geoid) %>% 
+      select(id,
+             AREACD = geoid,
+             AREANM = county_name)
+    
+    ### Export
+    sf_uhc %>% 
+      geojsonio::topojson_write(
+        file = "../public/data/geo_counties.json",
+        object_name  = 'geog')
+    
+    }
+  
+  { # data_state.csv ----------------------------------------------------------
+    
+    df_region %>% glimpse()
+    
+    ## Intermediates
+    df_demographics_tmp = df_demographics %>% 
+      filter(geo == 'state') 
+    
+    df_pop_wide = df_demographics_tmp %>% 
+      select(-median_age) %>% 
+      pivot_wider(names_from = year, values_from = pop) %>% 
+      drop_na() %>% 
+      mutate(`2001` = 0,
+             `2002` = 0,
+             `2003` = 0,
+             `2004` = 0,
+             `2005` = 0,
+             `2006` = 0,
+             `2007` = 0,
+             `2008` = 0) %>% 
+      select(sort(names(.))) %>% 
+      select(geo, state_fip = geoid, geoid, everything())
+    
+    
+    
+    ## Final
+    df_data_state = df_pop_wide %>% 
+      left_join(xwalk_state) %>% 
+      filter(state_abbr%in%vec__state_abbr) %>% 
+      select(code = state_fip,
+             name = state_name, 
+             # area = aland_mile2,
+             # density = pop_dens,
+             # age_med = median_age,
+             matches("\\b\\d{4}\\b"))
+    
+    ## Export
+    df_data_state %>% write_csv("../public/data/data_state.csv")
+    
+  }
+  
+  { # data_county.csv ----------------------------------------------------------
+    
+    df_district %>% glimpse()
+    
+    ## Intermediates
+    xwalk_county_state = sf_county_seed %>%
+      as.data.frame() %>% 
+      select(geoid, state_fip, county_name) %>% 
+      as_tibble()
+    
+    df_demographics_tmp = df_demographics %>% 
+      filter(geo == 'county') %>% 
+      left_join(xwalk_county_state) %>% 
+      left_join(xwalk_state) %>%
+      filter(state_abbr%in%vec__state_abbr) %>%
+      glimpse()
+    
+    df_pop_wide = df_demographics_tmp %>% 
+      select(-median_age) %>% 
+      pivot_wider(names_from = year, values_from = pop) %>% 
+      drop_na() %>% 
+      mutate(`2001` = 0,
+             `2002` = 0,
+             `2003` = 0,
+             `2004` = 0,
+             `2005` = 0,
+             `2006` = 0,
+             `2007` = 0,
+             `2008` = 0) %>% 
+      select(sort(names(.))) %>% 
+      select(geo, geoid, everything())
+    
+    df_spatial_metadata = sf_county_seed %>% 
+      as.data.frame() %>% 
+      as_tibble() %>% 
+      select(geoid, county_name,  aland_mile2,pop_dens) 
+    
+    df_age = df_demographics_tmp %>% 
+      filter(year == 2020) %>% 
+      select(geoid, median_age)
+    
+    ## Final
+    df_data_county = df_pop_wide %>% 
+      left_join(df_spatial_metadata) %>% 
+      left_join(df_age) %>% 
+      select(code = geoid,
+             name = county_name,
+             parent = state_fip,
+             area = aland_mile2,
+             density = pop_dens,
+             age_med = median_age,
+             matches("\\b\\d{4}\\b"))
+    
+    ## Export
+    df_data_county %>% write_csv("../public/data/data_county.csv")
+    
+  }
   
 }
--- .etl/raw/README.md
+++
@@ -1,4 +0,0 @@
-## Raw data
-
- - `NCHS_-_Death_rates_and_life_expectancy_at_birth.csv` was downloaded from data.gov:https://catalog.data.gov/dataset/nchs-death-rates-and-life-expectancy-at-birth
- - 
